7f529744726167b529e2673605b46dd90ef84af7
Tomasz Kaminski <tomasz.kaminski1025@gmail.com>
2022-04-17 00:38:46 +0100

    Added web scraping for checking verb conjugation.

--- German/learning_german.py
+++ German/learning_german.py
@@ -4,7 +4,7 @@
 from os.path import isfile, abspath, dirname
 from os import chdir, _exit
 from random import shuffle
-from collections import ChainMap
+from collections import ChainMap, defaultdict
 from itertools import permutations
 from datetime import datetime
 from re import sub
@@ -13,10 +13,34 @@ from time import sleep
 import json
 
 from playsound import playsound
+import requests
+from bs4 import BeautifulSoup
 
 from german_language import VERBS_DICT, ADVERBS_DICT
 
 ####################################################################################################
+
+def conjugate_verb(verb):
+    verb = verb.lower().replace("sich", "").strip()
+    page = requests.get("https://pl.pons.com/odmiana-czasownikow/niemiecki/" + verb)
+    parsed_page = BeautifulSoup(page.content, "html.parser")
+    if parsed_page.find("div", class_="alert alert-block alert-warning"):
+        print(f"Couldn't find verb {verb}, stopping.")
+        return False
+
+    conjugations = defaultdict(list)
+    for mood in parsed_page.find_all("section", class_="pons content-box ft-group"):
+        # Searching for h2 gives too many results here
+        mood_name = mood.find("span", "ft-current-header").text
+        if mood_name not in ["Indikativ", "Imperativ"]:
+            continue
+        for tense in mood.find_all("div", class_="ft-single-table"): # Tables
+            category = tense.find("h3").text if tense.find("h3") else mood_name
+            for person in tense.find_all("tr"): # Table rows
+                phrase = " ".join(word.text for word in person.find_all("td")) # Table cells
+                conjugations[category].append(phrase)
+    return conjugations
+
 
 def translate_words(vocab, direction):
     """_summary_
@@ -54,8 +78,7 @@ def translate_words(vocab, direction):
     vocab = list(ChainMap(*vocab).items())
     if direction == "german_to_english":
         # Removing round & square brackets and everything inside them. Also flipping the word order.
-        vocab = [(sub(r"s*[\[\(].+[\]\)]s*", "", item[1]), item[0])
-                        for item in vocab]
+        vocab = [(sub(r"\s*[\[\(].+[\]\)]\s*", "", item[1]), item[0]) for item in vocab]
     shuffle(vocab)
     vocab = sorted(vocab, key=lambda item: memory[item[1]] if item[1] in memory else "")
 
@@ -87,6 +110,7 @@ def translate_words(vocab, direction):
 ####################################################################################################
 
 if __name__ == "__main__":
+    print(conjugate_verb("ausgeben"))
     chdir(dirname(abspath(__file__)))
     window = tk.Tk()
     window.geometry("500x400")
@@ -102,15 +126,11 @@ if __name__ == "__main__":
     window.bind("<Return>", lambda _: pause_var.set(1))
     window.mainloop()
 
-#TODO: translate sentences, verb conjugation and tenses, useful knowledge, false friends
+#TODO: translate sentences, useful knowledge
 
 ####################################################################################################
 
     # Use https://freetts.com/Home/GermanTTS for pronunciation sound files, Vicki voice
-    #from re import sub, search
-    #import tkinter as tk
-    #from os import rename
-    #from glob import glob
     #all_files = glob("C:\\Users\\daiwe\\Downloads\\better_german-*.mp3")
     #all_files = sorted(all_files, key=lambda x: int(search(r"german-(\d+)\.mp3", x).group(1)))
     #names = iter(["empfehlen","abfahren","selten","dann","einladen","woher","fragen","hören","bleiben","erzählen","umlernen","statt","bauen","rufen","sich entspannen","verbieten","kaum","herunterladen","spenden","abhängen","riechen","wissen","mitnehmen","helfen","buchstabieren","wenn","als","genau","sich interessieren","warten","erklären","denken","spüren","werfen","sich freuen","manchmal","trinken","warum","wieso","versuchen","einschlafen","träumen","wegwerfen","sich treffen","laufen","also","wie","bieten","anbieten","fliegen","hoffen","wider","verraten","sich verspäten","verlieren","bekommen","sitzen","lesen","spannen","wollen","treffen","man","stehen","aussteigen","leben","verkaufen","lügen","was","arbeiten","wählen","auswählen","fast","dass","dürfen","aufwachen","sich erinnern","ob","schreiben","sich beeilen","erwarten","aufräumen","dort","schlafen","möchten","erkennen","ziehen","verlernen","achten","verspäten","einsteigen","aufladen","besonders","öffnen","wünschen","schauen","sehen","sollen","zählen","verschlafen","lernen","reden","holen","geben","weder noch","tun","sprechen","erleben","bezahlen","zeichnen","tragen","packen","stellen","dauern","können","erreichen","sofort","lösen","bestellen","sagen","umsteigen","beginnen","anfangen","waschen","wer","ohne","aufschließen","studieren","aber","bringen","aussehen","anschauen","sich ansehen","rennen","wieder","etwa","kosten","hängen","gehören","sich rasieren","suchen","immer","senden","schicken","oder","verstehen","etwas","sich fühlen","teilen","mögen","springen","bald","kochen","sich anziehen","sehr","nur","benutzen","anmachen","melden","machen","essen","gewinnen","ausgeben","einkaufen","versprechen","vorhaben","hochladen","wohnen","nie","niemals","vergleichen","brauchen","auch","vielleicht","hassen","anrufen","bereits","schon","wirklich","fahren","antworten","aufgeben","sich ausziehen","spielen","liegen","wann","trotz","beenden","besuchen","vergessen","kennen","laden","oft","üben","lehren","unterrichten","reinigen","verdienen","kommen","schließen","kaufen","reisen","müssen","ziemlich","beobachten","zeigen","ankommen","steigen","gehen","duschen","nehmen","wohin","wo","ausmachen","noch","jetzt","deshalb","deswegen","denn","weil","glauben","lieben","überdauern","probieren","eigentlich","schenken","losgehen","wiederholen","genug","zustimmen","bedeuten","obwohl","besitzen","erinnern"])
